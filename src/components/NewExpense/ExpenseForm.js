0872ca830b0900b90c8bd7a49c1b36f45af78142
Rafael Ferreira <rafaelferreirax3@gmail.com>
2023-04-08 21:15:58 -0300

    feat: add form validation

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,6 +6,10 @@ const ExpenseForm = (props) => {
   const [enteredDescription, setEnteredDescription] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+  const [titleIsValid, setTitleIsValid] = useState(true);
+  const [descriptionIsValid, setDescriptionIsValid] = useState(true);
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const [dateIsValid, setDateIsValid] = useState(true);
 
   const addYears = (date, years) => {
     date.setFullYear(date.getFullYear() + years);
@@ -16,23 +20,52 @@ const ExpenseForm = (props) => {
   let maxYearDate = addYears(new Date(), 3);
 
   const titleChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setTitleIsValid(true);
+    }
     setEnteredTitle(event.target.value);
   };
 
   const descriptionChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setDescriptionIsValid(true);
+    }
     setEnteredDescription(event.target.value);
   };
 
   const amountChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setAmountIsValid(true);
+    }
     setEnteredAmount(event.target.value);
   };
 
   const dateChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setDateIsValid(true);
+    }
     setEnteredDate(event.target.value);
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
+
+    if (enteredTitle.trim().length === 0) {
+      setTitleIsValid(false);
+      return;
+    }
+    if (enteredDescription.trim().length === 0) {
+      setDescriptionIsValid(false);
+      return;
+    }
+    if (enteredAmount.trim().length === 0) {
+      setAmountIsValid(false);
+      return;
+    }
+    if (enteredDate.trim().length === 0) {
+      setDateIsValid(false);
+      return;
+    }
 
     const expenseData = {
       title: enteredTitle,
@@ -51,24 +84,32 @@ const ExpenseForm = (props) => {
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Título</label>
+        <div
+          className={`new-expense__control ${!titleIsValid ? "invalid" : ""}`}
+        >
+          <label>Título *</label>
           <input
             type="text"
             value={enteredTitle}
             onChange={titleChangeHandler}
           />
         </div>
-        <div className="new-expense__control">
-          <label>Descrição</label>
+        <div
+          className={`new-expense__control ${
+            !descriptionIsValid ? "invalid" : ""
+          }`}
+        >
+          <label>Descrição *</label>
           <input
             type="text"
             value={enteredDescription}
             onChange={descriptionChangeHandler}
           />
         </div>
-        <div className="new-expense__control">
-          <label>Valor</label>
+        <div
+          className={`new-expense__control ${!amountIsValid ? "invalid" : ""}`}
+        >
+          <label>Valor *</label>
           <input
             type="number"
             min="0.01"
@@ -78,8 +119,10 @@ const ExpenseForm = (props) => {
             onChange={amountChangeHandler}
           />
         </div>
-        <div className="new-expense__control">
-          <label>Data</label>
+        <div
+          className={`new-expense__control ${!dateIsValid ? "invalid" : ""}`}
+        >
+          <label>Data *</label>
           <input
             type="date"
             min={currentDate}
